b7ba5ce20de8248e589e31d93a5db064991e829b
“mayuri” <mayurisalunkhe01@gmail.com>
2021-11-08 12:47:25 +0530

    Adding mood analyser

--- MoodCheckProgram20/moodAnalysis.cs
+++ MoodCheckProgram20/moodAnalysis.cs
@@ -6,8 +6,22 @@ using System.Threading.Tasks;
 
 namespace MoodCheckProgram20
 {
-    class moodAnalysis
+    public class moodAnalysis
     {
-        //reference
+        public string message;
+
+        public moodAnalysis(string message)
+        {
+            this.message = message;
+        }
+        public string AnalyseMood(string message)
+        {
+            if (message.ToLower().Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+                return "HAPPY";
+        }
     }
 }
---
+++ moodAnalysisTest/MoodTestCase.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodCheckProgram20;
+
+namespace moodAnalysisTest
+{
+    [TestClass]
+    public class MoodTestCase
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //TEST CASE-1
+
+            //Arrange
+            string message = "I am in sad mood";
+            string expected = "SAD";
+            //Act
+            moodAnalysis analyser = new moodAnalysis(message);
+            string actual = analyser.AnalyseMood(message);
+            //Act
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
